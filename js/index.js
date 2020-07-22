2175bf599f47cdb134b89f5784d9b03eec991d74
Devang K <52554056+rising-entropy@users.noreply.github.com>
2020-07-22 16:17:09 +0530

    v1.0

--- js/index.js
+++ js/index.js
@@ -7,18 +7,15 @@ $.getJSON(url,
         for (var i = 0; i < 30; i++) {
             urlLst.push(data[i].download_url);
              var linko = data[i].download_url;
-             //var linko2 = data[i+1].download_url;
              var auth = data[i].author;
-             //var auth2 = data[i+1].author;
-                 //$(".pictures").append("<div class='col-lg-6 col-md-6 col-sm-12'><img src='"+linko+"' class='img-fluid pic' data-wow-delay='0.3s' alt='"+auth+"'><img src='"+linko2+"' class='img-fluid pic' data-wow-delay='0.3s' alt='"+auth2+"'></div>");
-                 $(".pictures").append("<div class='col-lg-6 col-md-6 col-sm-12'><img src='"+linko+"' class='img-fluid pic' data-wow-delay='0.3s' alt='"+auth+"'></div>");
+                 $(".pictures").append("<div class='col-lg-6 col-md-6 col-sm-12'><img src='"+linko+"' onload='$(this).fadeIn(1000)'; class='img-fluid pic' data-wow-delay='0.3s' alt='"+auth+"'></div>");
         }
     }
 );
 
 
 
-
+onload='$(this).fadeIn()';
 
 
 $(".img-fluid").addClass("wow fadeIn z-depth-1-half");
---
+++ js/index3.js
@@ -0,0 +1,27 @@
+var url = "https://picsum.photos/v2/list?page=2&limit=100";
+
+
+$.getJSON(url,
+    function (data) {
+        console.log(data);
+        for (var i = 30; i < 60; i++) {
+             var linko = data[i].download_url;
+             var auth = data[i].author;
+             $(".pictures").append("<div class='col-lg-6 col-md-6 col-sm-12'><img src='"+linko+"' class='img-fluid pic' data-wow-delay='0.3s' alt='"+auth+"'></div>");
+        }
+    }
+);
+
+
+
+
+
+
+$(".img-fluid").addClass("wow fadeIn z-depth-1-half");
+
+
+
+
+
+
+
