23c4b5346fff52e8335682998964e93eba191907
Devang K <52554056+rising-entropy@users.noreply.github.com>
2020-07-22 12:45:42 +0530

    pls work dammit

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ $.getJSON(url,
              var linko2 = data[i+1].download_url;
              var auth = data[i].author;
              var auth2 = data[i+1].author;
-                 $(".pictures").append("<div class='col col-lg-4 col-md-6 col-sm-12'><img src='"+linko+"' class='img-fluid pic' data-wow-delay='0.3s' alt='"+auth+"'><img src='"+linko2+"' class='img-fluid pic' data-wow-delay='0.3s' alt='"+auth2+"'></div>");
+                 $(".pictures").append("<div class='col-lg-6 col-md-12 col-sm-12'><img src='"+linko+"' class='img-fluid pic' data-wow-delay='0.3s' alt='"+auth+"'><img src='"+linko2+"' class='img-fluid pic' data-wow-delay='0.3s' alt='"+auth2+"'></div>");
         }
     }
 );
